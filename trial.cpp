b5e76446608dce2f21820c685e01cbb2db372fd2
RoshanKumar404 <roshankuar951@gmail.com>
2024-08-28 21:09:00 +0530

    just started learning the OG c++

---
+++ trial.cpp
@@ -0,0 +1,31 @@
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+//     cout << "hello bro ";
+//     return 0;
+// }
+
+#include <cstdio>
+using namespace std;
+// void nahi(){
+//     puts("bas print kar do bina kisi return statement ke");
+// }
+// char character(){
+//     puts("it will print characters not the whole word with return statement of chatacter");
+//     return 'a';
+// }
+
+int main(){
+    puts("click me");
+    puts("click him");
+    puts("he is leaving your town");
+
+//     nahi();
+// character();
+    return 0 ;
+
+} 
+
+
+
