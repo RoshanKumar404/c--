8913f8a54924fef5a6516d85ffd010b88023dbb3
RoshanKumar404 <roshankuar951@gmail.com>
2024-08-28 21:27:50 +0530

    learned why we need namespaces

---
+++ inputOutput.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    std::cout<<"ji hujur \n";// is tarah se bhi hm next line print kr skte hai
+     cout<<"jai sri ram " <<std::endl; // or is tarike se bhi
+    return 0;
+}
+// in this we know that why we use namespace std 
