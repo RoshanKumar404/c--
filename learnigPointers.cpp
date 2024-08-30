6cd471fc2e17ddc5e82fb1c00f8829a0b3156cc3
RoshanKumar404 <roshankuar951@gmail.com>
2024-08-30 23:04:06 +0530

    learned refrencing and defrencing

--- learnigPointers.cpp
+++ learnigPointers.cpp
@@ -8,13 +8,26 @@ int main(){
 
 // printf("\n your memory address is %p", poin);
 
-int memAdd= 3403;
-int defmem=memAdd;
-int *memspointer=&memAdd;
-defmem=*memspointer;
-cout<<memAdd;
-printf("\n your memory address is %p  \n",memspointer);
-cout<<defmem;
+// int memAdd= 3403;
+// int defmem=memAdd;
+// int *memspointer=&memAdd;
+// defmem=*memspointer;
+// cout<<memAdd;
+// printf("\n your memory address is %p  \n",memspointer);
+// cout<<defmem;
 
+// referncing
+int score=38;
+int *mypointer=&score;
+
+printf("value of score in %d is \n", score);
+printf("value of the score which is pointed by  mypointer %p is \n" , mypointer
+);
+
+int &refrence= score;
+refrence=500;
+printf("value of score in %d is \n", score);
+printf("value of the score which is pointed by  mypointer %p is \n" , mypointer
+);
     return 0;
 }
