4766ebeb5c6466b977873e6788006638f6fc1377
RoshanKumar404 <roshankuar951@gmail.com>
2024-09-01 23:02:17 +0530

    made a program whicj tells the

---
+++ datatype.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"1 byte is equals to 8 bits \n";
+    printf("size of this double data type is %ld  bytes \n", sizeof(double));
+    printf("size of this  int data type is %ld  bytes \n", sizeof(int));
+    printf("size of this    char data type is %ld  bytes \n", sizeof(char));
+    printf("size of this     long data type is %ld  bytes \n", sizeof(long));
+    printf("size of this     long data long type is %ld  bytes \n", sizeof(long long));
+    printf("size of this     shortdata  int type is %ld  bytes \n", sizeof(short int));
+
+    return 0;
+}
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,16 +1,4 @@
 #include<iostream>
 using namespace std;
 int main(){
-int array[6]={1,2,3,4,5,6};
-// cout<<array[4]<<" value at %d index of array\n";
-// printf("value at %d index of the array \n",array[4]);
-// cout<<array<<"memory address of the array \n";
-int second_array[4];
-second_array[2]=3;
-// cout<<second_array<<" SECOND ARRAY  memory addr.
-
-cout<<second_array[0]<<" SECOND ARRAY content at index 0 without providing any value \n ";
-*second_array=40;
-
-// cout<<second_array;
-cout<<second_array[0]<<" updated value with the help of  pointer, at index 0 \n ";
+    cout<<"1 byte is equals to 8 bits \n";
