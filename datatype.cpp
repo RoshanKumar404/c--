06781d88af29893d0d60d4660520dd7af5851caa
RoshanKumar404 <roshankuar951@gmail.com>
2024-09-01 23:11:39 +0530

    made a c++ program with basic to get the size of t of the datatype

--- datatype.cpp
+++ datatype.cpp
@@ -8,6 +8,9 @@ int main(){
     printf("size of this     long data type is %ld  bytes \n", sizeof(long));
     printf("size of this     long data long type is %ld  bytes \n", sizeof(long long));
     printf("size of this     shortdata  int type is %ld  bytes \n", sizeof(short int));
-
+    cout<<"enter the  data type:  ";
+    string datatype;
+    cin>>datatype;
+printf("the size of the data type %ld is ", sizeof(datatype));
     return 0;
 }
