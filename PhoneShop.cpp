8f3220dff869fff4917d28b83a9044fed50c1ea0
Roshankumar404 <roshankuar951@gmail.com>
2024-11-25 19:27:46 +0530

    no idea

---
+++ PhoneShop.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+cout<<"Welcome to the smartPhones Hub \n";
+cout<<"Customer : Namste sir \n" <<" Owner : Namste Sir , kahiye sir kya sewa kare aaka"<<endl;
+cout<<"sir aap maereko ku6 phones dikha do \n";
+cout<<"Range ya fir aap RAM aur kon sa company ka chahiye ye bta dijiye \n";
+int range;
+cin>>range;
+// string Company_name;//cin>>Company_name;
+
+while(range>1)
+{
+    cout<<"nahi hai \n";
+range--;
+    
+}
+
+
+
+
+
+
+return 0;
+}
