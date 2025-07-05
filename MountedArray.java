050813a4d262ebbf7a29bdba2adb0633b165134a
Roshankumar404 <roshankuar951@gmail.com>
2025-07-05 21:01:11 +0530

    pek mountain array

---
+++ MountedArray.java
@@ -0,0 +1,17 @@
+public class MountedArray {
+    
+    public int PeakIndexArray(int []arr){
+        int start=0;
+        int end=arr.length-1;
+        while (start<end) {
+            int mid=start+(end-start)/2;
+            if (arr[mid]>arr[mid+1]) {
+                end=mid;
+
+            }else{
+                start=mid+1;
+            }
+        }
+        return start;
+    }
+}
