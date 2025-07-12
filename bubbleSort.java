a155ea9a33ac8645477c19bf0e99b0d15a13c95a
Roshankumar404 <roshankuar951@gmail.com>
2025-07-12 21:29:45 +0530

    added  safety check to counter further swappings

--- bubbleSort.java
+++ bubbleSort.java
@@ -8,19 +8,26 @@ public class bubbleSort {
     }
 
     static void sort(int[] arr) {
+        boolean swapped;
         // we have to run the outer loop or steps n-1 times
         for (int i = 0; i < arr.length; i++) {
             // now we have to run the loop till lenght-i and compare the adjascent number
             // and hence the max number will come to last postion
+            swapped=false;
             for (int j = 1; j < arr.length - i; j++) {
                 if (arr[j] < arr[j-1]) {
                     int temp = arr[j];
                     arr[j] = arr[j - 1];
                     arr[j - 1] = temp;
+                    swapped=true;
 
                 }
 
             }
+            if (!swapped) {
+                break;
+            }
+            
         }
     }
 }
