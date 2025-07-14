0d0cffa2f2388645fd79737f25989536d606331c
Roshankumar404 <roshankuar951@gmail.com>
2025-07-14 22:26:30 +0530

    learning selection sort

---
+++ SelectionSort.java
@@ -0,0 +1,17 @@
+public class SelectionSort {
+    public static void main(String[] args) {
+        int []arr={3,5,2,6,2,0,8};
+System.out.println(SearchMax(arr, 0, 0));
+    }
+    
+    static int SearchMax(int[]arr,int start,int end){
+int max= start;
+       for (int i = start; i <end; i++) {
+        if (arr[max]<arr[i]) {
+            max=i;
+        }
+       }
+       return max;
+    }
+    
+}
