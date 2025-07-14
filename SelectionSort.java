016ed4ca8b41b490313149ea6fd2cb794afdf3a3
Roshankumar404 <roshankuar951@gmail.com>
2025-07-14 22:28:10 +0530

    logic i have to write

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,17 +1,20 @@
 public class SelectionSort {
     public static void main(String[] args) {
-        int []arr={3,5,2,6,2,0,8};
-System.out.println(SearchMax(arr, 0, 0));
+        int[] arr = { 3, 5, 2, 6, 2, 0, 8 };
+        System.out.println(SearchMax(arr, 0, 0));
     }
-    
-    static int SearchMax(int[]arr,int start,int end){
-int max= start;
-       for (int i = start; i <end; i++) {
-        if (arr[max]<arr[i]) {
-            max=i;
+
+    static int SearchMax(int[] arr, int start, int end) {
+        int max = start;
+        for (int i = start; i < end; i++) {
+            if (arr[max] < arr[i]) {
+                max = i;
+            }
         }
-       }
-       return max;
+        return max;
     }
-    
+
+    static void swap(int[]arr,int start,int end){
+        
+    }
 }
