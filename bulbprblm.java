eb0f8fcd710b13eac45adebedc5ac87a5d31ff59
Roshankumar404 <roshankuar951@gmail.com>
2025-07-21 18:51:34 +0530

    jh

---
+++ bulbprblm.java
@@ -0,0 +1,16 @@
+public class bulbprblm {
+public int bulb(int n) {
+    int count=0;
+
+    int i=1;
+    while (i*i<=n) {
+        count++;
+        i++;
+
+        
+    }
+    return count;
+    
+}
+}
+   
