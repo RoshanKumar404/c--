68c361cf33e5c37d6b216bfb167bd36b74d8e6b7
Roshankumar404 <roshankuar951@gmail.com>
2025-07-28 23:27:04 +0530

    nothing

---
+++ PlaceHolder.java
@@ -0,0 +1,11 @@
+public class PlaceHolder {
+    
+    public static void main(String[] args) {
+        System.out.println("this is for float placce older in printf");
+        float r= 23.23f;
+        System.out.printf("float formatted numbre is %.2f ", r);
+        System.out.println();
+
+        System.out.printf("Hello my name is %s and i am %s ","Roshan", "Mr. Perfect");
+    }
+}
