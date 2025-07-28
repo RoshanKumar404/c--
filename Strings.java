7e29ee9f6b09fd5652fc0fb696c9093b719b9d80
Roshankumar404 <roshankuar951@gmail.com>
2025-07-28 22:12:20 +0530

    two different vaiables with same value has same pointer to point in string pool of heap memory

---
+++ Strings.java
@@ -0,0 +1,9 @@
+public class Strings {
+    public static void main(String[] args) {
+        String a= "rohit";
+        String v= new String ("rohit");
+        System.out.println(a==v);
+        System.out.println(a.equals(v));
+    }
+
+}//function overloading
---
+++ logestprefixTwoArr.java
@@ -0,0 +1,12 @@
+public class logestprefixTwoArr {
+    public static void main(String[] args) {
+         int []str={232,43,54,56,43};
+             String[] arr=new str.toString();
+System.out.println(arr);
+    }
+   
+    
+    
+}
+// static String arr(int []str){
+// }
