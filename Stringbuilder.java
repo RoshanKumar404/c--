c988e4767a06f8d5a6328d69cf927fdecb9569c5
Roshankumar404 <roshankuar951@gmail.com>
2025-08-01 17:31:47 +0530

    splitted

--- Stringbuilder.java
+++ Stringbuilder.java
@@ -1,10 +1,19 @@
 public class Stringbuilder {
     public static void main(String[] args){
-        StringBuilder builder=new StringBuilder();
-        for(int i=0;i<26;i++){
-            char ch= (char)('a'+i);
-            builder.append(ch);
-        }
-        System.out.println(builder);
+        // StringBuilder builder=new StringBuilder();
+        // for(int i=0;i<26;i++){
+        //     char ch= (char)('a'+i);
+        //     builder.append(ch);
+        // }
+        // System.out.println(builder);
+String name= "ROshan kumar";
+String[] Splitted =name.split(" ");
+for(String split:Splitted){
+    System.out.println(split);
+
+}
+System.out.println(Splitted);
+
+
     }
 }
