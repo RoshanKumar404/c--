32a056dadc6934717b3751f93b8d1069e6f13e90
Roshankumar404 <roshankuar951@gmail.com>
2025-08-12 11:44:02 +0530

    tried using hash map but the normal method was  efficient

--- HashMap/CountPrefix.java
+++ HashMap/CountPrefix.java
@@ -1,21 +1,37 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class CountPrefix {
     public static void main(String[] args) {
-        String[] words={"a","b","c","ab","bc","abc"};
-        String s="abc";
+        String[] words = { "a", "b", "c", "ab", "bc", "abc" };
+        String s = "abc";
         System.out.println(countPrefix(words, s));
     }
-    public static int countPrefix(String[] words,String s){
-        int count=0;
+
+    public static int countPrefix(String[] words, String s) {
+
+        int count = 0;
+       Map<String, Integer> PreWords = new HashMap<>();
         for (int i = 0; i < words.length; i++) {
-            String prefix=words[i];
+            PreWords.put(words[i], i);
+        }
+        for (String prefix : PreWords.keySet()) {
             if (s.startsWith(prefix)) {
                 count++;
             }
-            
         }
+
+        // return count;
+        // for (int i = 0; i < words.length; i++) {
+        // String prefix=words[i];
+        // if (s.startsWith(prefix)) {
+        // count++;
+        // }
+
+        // }
 
         return count;
 
     }
-    
+
 }
