a350a0b8f806b153463b65e79b695842ae985dcf
Roshankumar404 <roshankuar951@gmail.com>
2025-08-14 12:29:53 +0530

    just revised

--- HashMap/wordPattern.java
+++ HashMap/wordPattern.java
@@ -6,46 +6,31 @@ public class wordPattern {
     public static void main(String[] args) {
           String pattern="abba";
     String s="dog cat cat dog";
-    System.out.println(pattern.toCharArray());
-    System.out.println(Arrays.toString(s.toCharArray()));
+    // System.out.println(pattern.toCharArray());
+    // System.out.println(Arrays.toString(s.toCharArray()));
+    System.out.println(wordpattern(s, pattern));
     }
     public static  boolean wordpattern(String s, String pattern){
         String [] words=s.split(" ");
-        char [] characters= Arrays.toString(pattern);
-        if (words.length!=characters.length||words.length==0) {
+       // char [] ch= Arrays.toString(pattern);
+        if (words.length!=pattern.length()||words.length==0) {
             return false;
         }
-        Map<String,Character> patternmatching=new HashMap<>();
-        for (int i = 0; i < characters.length; i++) {
-           patternmatching.put(words[i], characters[i]);
-        }
-        
-    }
-  
-    /*
-     * 
-     *    String[] words = s.split(" ");
-        if (words.length != pattern.length()) return false;
-
-        Map<String, Character> wordToChar = new HashMap<>();
-        Map<Character, String> charToWord = new HashMap<>();
-
+        Map<String,Character> wordTOChar=new HashMap<>();
+        Map<Character,String> CharToWord=new HashMap<>();
         for (int i = 0; i < words.length; i++) {
-            String word = words[i];
-            char ch = pattern.charAt(i);
+         String word= words[i];
+         char ch =pattern.charAt(i);
 
-            // Put mappings if absent
-            wordToChar.putIfAbsent(word, ch);
-            charToWord.putIfAbsent(ch, word);
-
-            // Check if existing mappings match
-            if (wordToChar.get(word) != ch || !charToWord.get(ch).equals(word)) {
-                return false;
-            }
+         wordTOChar.putIfAbsent(word,ch);
+         CharToWord.putIfAbsent(ch,word);
+         if (wordTOChar.get(word)!=ch|| !CharToWord.get(ch).equals(word)) {
+            return false;
+                 }
         }
-        return true;
-     */
 
+        return true;
+    }
   
 }
 
