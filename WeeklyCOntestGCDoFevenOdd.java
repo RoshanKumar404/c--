c38d4790a674601eac9e1ab579376a75b4fc30cd
Roshankumar404 <roshankuar951@gmail.com>
2025-08-29 23:33:03 +0530

    bas yahi kr skte thee without any logic

--- WeeklyCOntestGCDoFevenOdd.java
+++ WeeklyCOntestGCDoFevenOdd.java
@@ -1,37 +1,43 @@
 public class WeeklyCOntestGCDoFevenOdd {
 public static void main(String[] args) {
     int n=4;
-System.out.println(HCF(Even(n), Odd(n)));
+ System.out.println(HCF(n));
+// return n;
 }
-static int Even(int n){
-    //    int EvenSum=0;
-    //    for (int i=1;i<=n;i++){
-    //        if(i%2==0){
-    //            EvenSum+=i;
-    //        }
-    //    }
-    //    return EvenSum;
-        return n*(n+1);
-    }
-    static int Odd(int n){
-    //    int OddSum=0;
-    //    for (int i=1;i<=n;i++){
-    //        if(i%2!=0){
-    //            OddSum+=i;
-    //        }
-    //    }
-    //    return OddSum;
-        return n*n;
-    }
+// static int Even(int n){
+//     //    int EvenSum=0;
+//     //    for (int i=1;i<=n;i++){
+//     //        if(i%2==0){
+//     //            EvenSum+=i;
+//     //        }
+//     //    }
+//     //    return EvenSum;
+//         return n*(n+1);
+//     }
+//     static int Odd(int n){
+//     //    int OddSum=0;
+//     //    for (int i=1;i<=n;i++){
+//     //        if(i%2!=0){
+//     //            OddSum+=i;
+//     //        }
+//     //    }
+//     //    return OddSum;
+//         return n*n;
+//     }
 
-    public static int HCF(int a,int b) {
-        while (b != 0) {
-            int temp = b;
-            b = a % b;
-            a = temp;
+//     public static int HCF(int a,int b) {
+//         while (b != 0) {
+//             int temp = b;
+//             b = a % b;
+//             a = temp;
 
-        }
-        return a;
-    }
+//         }
+//         return a;
+//     }
+static int HCF(int n){
+    return n;
+}
+
     
 }
+//bas yahi karna tha
