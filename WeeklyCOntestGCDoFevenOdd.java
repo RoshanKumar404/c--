c4f07c6e14f76e3c43998f453a7757bcc85a0b46
Roshankumar404 <roshankuar951@gmail.com>
2025-08-29 23:12:04 +0530

    solved first weekly contest

---
+++ WeeklyCOntestGCDoFevenOdd.java
@@ -0,0 +1,37 @@
+public class WeeklyCOntestGCDoFevenOdd {
+public static void main(String[] args) {
+    int n=4;
+System.out.println(HCF(Even(n), Odd(n)));
+}
+static int Even(int n){
+       int EvenSum=0;
+       for (int i=1;i<=n;i++){
+           if(i%2==0){
+               EvenSum+=i;
+           }
+       }
+       return EvenSum;
+       // return n*(n+1);
+    }
+    static int Odd(int n){
+       int OddSum=0;
+       for (int i=1;i<=n;i++){
+           if(i%2!=0){
+               OddSum+=i;
+           }
+       }
+       return OddSum;
+       // return n*n;
+    }
+
+    public static int HCF(int a,int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+
+        }
+        return a;
+    }
+    
+}
