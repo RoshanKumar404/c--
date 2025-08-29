20d1cb7b187b1afcc0bc7ca6de48d534c5102868
Roshankumar404 <roshankuar951@gmail.com>
2025-08-29 23:12:51 +0530

    made some changes to work according to question

--- WeeklyCOntestGCDoFevenOdd.java
+++ WeeklyCOntestGCDoFevenOdd.java
@@ -4,24 +4,24 @@ public static void main(String[] args) {
 System.out.println(HCF(Even(n), Odd(n)));
 }
 static int Even(int n){
-       int EvenSum=0;
-       for (int i=1;i<=n;i++){
-           if(i%2==0){
-               EvenSum+=i;
-           }
-       }
-       return EvenSum;
-       // return n*(n+1);
+    //    int EvenSum=0;
+    //    for (int i=1;i<=n;i++){
+    //        if(i%2==0){
+    //            EvenSum+=i;
+    //        }
+    //    }
+    //    return EvenSum;
+        return n*(n+1);
     }
     static int Odd(int n){
-       int OddSum=0;
-       for (int i=1;i<=n;i++){
-           if(i%2!=0){
-               OddSum+=i;
-           }
-       }
-       return OddSum;
-       // return n*n;
+    //    int OddSum=0;
+    //    for (int i=1;i<=n;i++){
+    //        if(i%2!=0){
+    //            OddSum+=i;
+    //        }
+    //    }
+    //    return OddSum;
+        return n*n;
     }
 
     public static int HCF(int a,int b) {
