bba75c1fdf5fa7cd6df1fa9664e784f7bf7723fe
Roshankumar404 <roshankuar951@gmail.com>
2025-11-10 09:52:51 +0530

    factorial and backward sum till n

--- Recursion/RecursionNto1.java
+++ Recursion/RecursionNto1.java
@@ -1,14 +1,16 @@
 public class RecursionNto1 {
     public static void main(String[] args) {
-        fun(9);
+        fun(5);
     }
     static void fun(int n){
         if(n==0){
             return;
         }
         System.out.println(n);
-        fun(n-1);
-        System.out.println(n);
+         fun(n-1);
+     //this is woring logic:-   System.out.println(fun(n-1)*n);
+       
+       System.out.println(n);
     }
     
 }
