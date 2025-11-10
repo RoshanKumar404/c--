0119998eb14776a673ec9cef187eecd912e84906
Roshankumar404 <roshankuar951@gmail.com>
2025-11-10 13:46:23 +0530

    did pallindrome using recursion

--- Recursion/ReverseNumberRecursion.java
+++ Recursion/ReverseNumberRecursion.java
@@ -1,17 +1,26 @@
 public class ReverseNumberRecursion {
     public static void main(String[] args) {
-        System.out.println(reverse(12345));
+       // int num=1232;
+        System.out.println(revers(121));
+       // System.out.println(reverse(12345));
     }
-    static int  reverse(int n){
+    static boolean  revers(int n){
         int digits=(int)(Math.log10(n))+1;
-        return reverser(n,digits);
+        int ans= reverser(n,digits);
+        // if(n==ans){return true;
+        // }
+        return n==ans;
+      
     }
     private static int reverser(int n, int digits){
-        if(n/10==n){
+        if(n<10){
             return n;
 
         }
         int remainder=n%10;
         return remainder*(int)(Math.pow(10, digits-1))+reverser(n/10,digits-1);
+        // if (n==n){
+        //     return true;
+        // } 
     }
 }
