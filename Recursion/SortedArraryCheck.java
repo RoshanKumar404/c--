abe09491615c07adb911c79c65a4ef5a0363435a
Roshankumar404 <roshankuar951@gmail.com>
2025-11-12 08:43:58 +0530

    this checks the array in descending order

--- Recursion/SortedArraryCheck.java
+++ Recursion/SortedArraryCheck.java
@@ -1,6 +1,6 @@
 public class SortedArraryCheck {
     public static void main(String[] args) {
-        int []arr={3,4,5,6,7,5,9};
+        int []arr={9,8,7,4,3};
         System.out.println(isSorted(arr,0));
 
     }
@@ -8,6 +8,6 @@ public class SortedArraryCheck {
         if (i==arr.length-1) {
             return true;
         }
-        return arr[i]<arr[i+1] && isSorted(arr, i+1);
+        return arr[i]>arr[i+1] && isSorted(arr, i+1);
     }
 }
