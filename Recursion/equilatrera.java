fb2ffd09ff7129ce8c0992f719667cf2b943cceb
Roshankumar404 <roshankuar951@gmail.com>
2025-11-18 15:31:47 +0530

    did selection sort using recursion

--- Recursion/equilatrera.java
+++ Recursion/equilatrera.java
@@ -1,24 +1,28 @@
 public class equilatrera {
     
     public static void main(String[] args) {
-        upside(0,5, 0);
+        upside(5,0, 0);
     }
     static void upside(int row,int col,int space){
         if (row==0) {
             return ;
         }
-        if (space<(col-row)) {
-            System.out.println(" ");
+        if (space<(row-col)) {
+            System.out.print(" ");
             upside(row, col, space+1);
             
         }
         else if(col<(2*row-1)){
-            System.out.println("*");
-            upside(row, col+1, space);
+           
+            
+             System.out.print("*");
+             upside(row, col+1, space);
         }
         else{
-            System.out.println();
-            upside(row-1, 0, 0);
+           
+            
+             System.out.println();
+             upside(row-1, 0, 0);
         }
 
     }
