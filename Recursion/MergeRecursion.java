d7133b832f02d9816993ccf8df293e4e01df03fb
Roshankumar404 <roshankuar951@gmail.com>
2025-11-19 10:00:09 +0530

    completed

--- Recursion/MergeRecursion.java
+++ Recursion/MergeRecursion.java
@@ -2,36 +2,46 @@ import java.util.Arrays;
 
 public class MergeRecursion {
     public static void main(String[] args) {
-        int []arr={5,4,3,2,1};
-        int ans=mergesort(arr);
-        System.out.println(Arrays.toString(arr));
+        int[] arr = {5, 4, 3, 2, 1};
+        int[] ans = mergesort(arr);
+        System.out.println(Arrays.toString(ans));
     }
-    static int [] mergesort(int[]arr){
-        if (arr.length==1) {
+
+    static int[] mergesort(int[] arr) {
+        if (arr.length == 1) {
             return arr;
         }
-        int mid=arr.length/2;
-        int[] LeftPart= mergesort(Arrays.copyOfRange(arr,0,mid));
-        int []RightPart= mergesort(Arrays.copyOfRange(arr,mid,arr.length));
-        return merger(LeftPart,RightPart);
+
+        int mid = arr.length / 2;
+
+        int[] LeftPart = mergesort(Arrays.copyOfRange(arr, 0, mid));
+        int[] RightPart = mergesort(Arrays.copyOfRange(arr, mid, arr.length));
+
+        return merger(LeftPart, RightPart);
     }
-    static int[]merger(int[]FirstArray,int[] secondArray){
-        int[] mergedArray=new int[FirstArray.length+secondArray.length];
-        int i=0;
-        int j=0;
-        int k=0;
-        while (i<FirstArray.length && secondArray.length>j) {
-            if (FirstArray[i]<secondArray[j]) {
-                merger[k]=FirstArray[i];
-                i++;
-                
-            }else{
-                merger[k]=secondArray[j];
-                j++;
+
+    static int[] merger(int[] FirstArray, int[] secondArray) {
+        int[] mergedArray = new int[FirstArray.length + secondArray.length];
+        int i = 0, j = 0, k = 0;
+
+        // merging two sorted arrays
+        while (i < FirstArray.length && j < secondArray.length) {
+            if (FirstArray[i] < secondArray[j]) {
+                mergedArray[k++] = FirstArray[i++];
+            } else {
+                mergedArray[k++] = secondArray[j++];
             }
-            return merger;
-            
         }
 
+        // leftover elements
+        while (i < FirstArray.length) {
+            mergedArray[k++] = FirstArray[i++];
+        }
+
+        while (j < secondArray.length) {
+            mergedArray[k++] = secondArray[j++];
+        }
+
+        return mergedArray;
     }
 }
