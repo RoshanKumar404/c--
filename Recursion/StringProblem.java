b3eb20657bf1f586377f69d77b8a190b83bc36ea
Roshankumar404 <roshankuar951@gmail.com>
2025-11-20 10:51:15 +0530

    did with a return statement too

--- Recursion/StringProblem.java
+++ Recursion/StringProblem.java
@@ -1,23 +1,41 @@
 public class StringProblem {
 
     public static void main(String[] args) {
-      //System.out.println(cahnger("", "afavsa"));
-   changer("avasfvea","");
-    // System.out.println(d);
+        // System.out.println(cahnger("", "afavsa"));
+        changer("avasfvea", "");
+        // System.out.println(d);
+        System.out.println("next type");
+        System.out.println(StringWithReturnType("afdasgvsa"));
     }
-    static void changer(String before, String after){
+
+    static void changer(String before, String after) {
         if (before.isEmpty()) {
-           System.out.println(after);
+            System.out.println(after);
             return;
         }
-        char ch= before.charAt(0);
-        if (ch=='a') {
-            changer( before.substring(1),after);
+        char ch = before.charAt(0);
+        if (ch == 'a') {
+            changer(before.substring(1), after);
 
-        }else{ 
-            
-            changer(before.substring(1), after+ch);
+        } else {
+
+            changer(before.substring(1), after + ch);
         }
     }
-    
+
+    static String StringWithReturnType(String before) {
+        if (before.isEmpty()) {
+            // System.out.println(after);
+            return "";
+        }
+        char ch = before.charAt(0);
+        if (ch == 'a') {
+            return StringWithReturnType(before.substring(1));
+
+        } else {
+
+            return ch + StringWithReturnType(before.substring(1));
+        }
+    }
+
 }
