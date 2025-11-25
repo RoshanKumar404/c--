c8a9757bcab94e68f01219f74034714afac42594
Roshankumar404 <roshankuar951@gmail.com>
2025-11-25 11:45:09 +0530

    used Array lsit

--- Recursion/SubsetsRecursion.java
+++ Recursion/SubsetsRecursion.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+
 public class SubsetsRecursion {
     public static void main(String[] args) {
         subsets("", "abc");
+        System.out.println(Subseq("","abcd"));
     }
 
     static void subsets(String processed, String Unprocessed){
@@ -12,6 +15,19 @@ public class SubsetsRecursion {
         
         subsets(processed+ch, Unprocessed.substring(1));
         subsets(processed, Unprocessed.substring(1));
+    }
+    static ArrayList<String> Subseq(String processed,String Unprocessed){
+        if (Unprocessed.isEmpty()) {
+            ArrayList<String>list= new ArrayList<>();
+            list.add(processed);
+            return list;
+        
+        }
+        char ch= Unprocessed.charAt(0 );
+        ArrayList<String>Lefttree= Subseq(processed, Unprocessed.substring(1));
+        ArrayList<String>RightTree=Subseq(processed+ch,Unprocessed.substring(1));
+        Lefttree.addAll(RightTree);
+        return Lefttree;
     }
     
 }
