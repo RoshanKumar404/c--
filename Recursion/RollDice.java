a8e521d2fecb6062874734153b74dec3cdd5f0d9
Roshankumar404 <roshankuar951@gmail.com>
2025-12-14 11:54:11 +0530

    dadsd

--- Recursion/RollDice.java
+++ Recursion/RollDice.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+
 public class RollDice {
     public static void main(String[] args) {
         dice(4, "");
+        System.out.println(ROllreturn("",4).size());
     }
     
     static void dice(int target, String pro){
@@ -11,6 +14,21 @@ public class RollDice {
         for(int i =1; i<=6&&i<=target;i++){
             dice(target-i, pro+i);
         }
+    }
+    static ArrayList<String> ROllreturn(String pr, int target){
+        if (target==0) {
+            ArrayList<String>list=new ArrayList<>();
+            list.add(pr);
+            System.out.println(pr);
+            return list;
+        }
+        ArrayList<String>list= new ArrayList<>();
+        for(int i=1;i<=6&&i<=target;i++){
+           
+            list.addAll( ROllreturn(pr+i,target-i));
+        }
+        return list;
+
     }
     
 }
