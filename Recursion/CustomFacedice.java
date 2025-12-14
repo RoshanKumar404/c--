8334b9311f9167c5cd614cc1f030a3d447790c3a
Roshankumar404 <roshankuar951@gmail.com>
2025-12-14 12:19:39 +0530

    got the result as list

--- Recursion/CustomFacedice.java
+++ Recursion/CustomFacedice.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
+
 public class CustomFacedice {
     public static void main(String[] args) {
         int[]faces={1,3,5};
         dice(faces,5,"");
+        System.out.println(Dicereturn("", faces, 4));
         
     }
     static void dice(int []faces,int target,String pr){
@@ -15,5 +18,19 @@ public class CustomFacedice {
                 dice(faces,target-face,pr+face);
             }
         }
+    }
+    static ArrayList<String>Dicereturn(String pr,int [] faces,int target){
+        if (target==0) {
+            ArrayList<String>list= new ArrayList<>();
+            list.add(pr);
+            return list;
+        }
+        ArrayList<String>result=new ArrayList<>();
+        for(int face:faces){
+            if (face<=target) {
+                result.addAll(Dicereturn(pr+face, faces, target-face));
+            }
+        }
+        return result;
     }
 }
