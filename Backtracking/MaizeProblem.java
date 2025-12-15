214ae6c98ca2bf913b10e546baa3ca99a044be1c
Roshankumar404 <roshankuar951@gmail.com>
2025-12-15 11:29:35 +0530

    printed the path of  result

--- Backtracking/MaizeProblem.java
+++ Backtracking/MaizeProblem.java
@@ -1,6 +1,7 @@
 public class MaizeProblem {
     public static void main(String[] args) {
        System.out.println(maizeCount(3,3));
+       path("", 3, 3);
     }
 
     static int maizeCount(int row, int column){
@@ -10,6 +11,18 @@ public class MaizeProblem {
         int left= maizeCount(row-1, column);
         int right= maizeCount(row, column-1);
         return left+right;
+    }
+    static void path(String Pr,int row,int column){
+        if (row==1&&column==1) {
+            System.out.println(Pr);
+            return;
+        }
+        if (row>1) {
+            path(Pr+'d', row-1, column);
+        }
+        if (column>1) {
+            path(Pr+'r', row, column-1);
+        }
     }
     
 }
