cfc3841230c75f4827cb2c07d822807aea3e300f
Roshankumar404 <roshankuar951@gmail.com>
2025-12-31 14:32:52 +0530

    this is how i displayed

--- LinkedList/LLMain.java
+++ LinkedList/LLMain.java
@@ -1,3 +1,13 @@
 public class LLMain {
+     public static void main(String[] args) {
+    LLBase list= new LLBase();
+
+    list.InsertFirst(32);
+    list.InsertFirst(42);
+    list.InsertFirst(23);
+    list.InsertFirst(2);
+    list.InsertFirst(13);
+    list.display();
+   }
   
 }
