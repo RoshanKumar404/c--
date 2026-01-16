95390ecbbbaa55240cdaf7560bbb6c452ebd6f68
Roshankumar404 <roshankuar951@gmail.com>
2026-01-16 22:25:14 +0530

    created the format of Double likned list

--- LinkedList/DoubleLL.java
+++ LinkedList/DoubleLL.java
@@ -1,4 +1,4 @@
-public abstract class DoubleLL {
+public  class DoubleLL {
     // In this only the node.prev i sneing added
      /*
       for eg node.next= head;
@@ -6,5 +6,24 @@ public abstract class DoubleLL {
       node.prev=null;
       node= head;
       just it
+      it is only benefitial in backward traversal
+
      */
+
+      private class  Node {
+        int val;
+        Node next;
+        Node prev;
+
+        public Node(int val){
+            this.val=val;
+        }
+        public Node(int val,Node next, Node prev){
+            this.val=val;
+            this.next=next;
+            this.prev=prev;
+        }
+      
+        
+      }
 }
