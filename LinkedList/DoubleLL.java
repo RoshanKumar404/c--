c65a88834e2ee7e59a9d13a9beeddc0141addc2b
Roshankumar404 <roshankuar951@gmail.com>
2026-01-16 23:00:40 +0530

    tried to printing the list in reverse

--- LinkedList/DoubleLL.java
+++ LinkedList/DoubleLL.java
@@ -25,13 +25,25 @@ public class DoubleLL {
         head = node;
 
     }
-    public void display(){
-        Node node= head;
-        while (node!=null) {
-            System.out.print(node.val +  " -> ");
-            node=node.next;
+
+    public void display() {
+        Node node = head;
+        Node last = null;
+
+        while (node != null) {
+            System.out.print(node.val + " -> ");
+            last = node;
+            node = node.next;
         }
         System.out.println("Null");
+        System.out.println(
+                "printing reverse");
+        while (last != null) {
+            System.out.print(last.val + " -> ");
+            last = last.prev;
+
+        }
+        System.out.print("start");
     }
 
     private class Node {
