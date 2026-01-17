25c2556a55c6daa9e6b2c522da8a49902168e9b7
Roshankumar404 <roshankuar951@gmail.com>
2026-01-17 21:51:27 +0530

    its inserting if there is no node i.e null

--- LinkedList/DoubleMain.java
+++ LinkedList/DoubleMain.java
@@ -1,14 +1,14 @@
 public class DoubleMain {
    public static void main(String[] args) {
     DoubleLL list= new DoubleLL();
-    list.InsertFirst(2);
-    list.InsertFirst(5);
+   //  list.InsertFirst(2);
+   //  list.InsertFirst(5);
 
    //  list.InsertFirst(26);
    //  list.insertLLast(54);
 
-    list.InsertFirst(20);
-    list.insertLLast(32);
+   //  list.InsertFirst(20);
+   //  list.insertLLast(32);
     list.InsernextTo(25,3);
     list.display();
 
