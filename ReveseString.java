ecdc95a25b2658548b30babe890d23004d17f8ac
Roshankumar404 <roshankuar951@gmail.com>
2026-02-13 15:11:18 +0530

    this is the solution of leetcode 511

--- ReveseString.java
+++ ReveseString.java
@@ -1,13 +1,22 @@
 public class ReveseString {
     public static void main(String[] args) {
-        char[]s={'h','e','y'};
-        ReverseString(s);
-        System.out.println(java.util.Arrays.toString(s));
+        char[]s={'h','e','l','k','y',};
+        String ss="abcdefg";
+        int k=2;
+        String result=ReverseString(ss,k);
+        System.out.println(result);
+        // System.out.println(java.util.Arrays.toString(s));
     }
-    public static void ReverseString(char[]s){
-    int start=0;
-    int end=s.length-1;
+    public static String ReverseString(String ss,int k){
+    char[]s =ss.toCharArray();
+    for (int i = 0; i < s.length; i+=2*k) {
+        
+    
+    int start=i;
+    int end=Math.min(i+k-1, ss.length()-1);
+    
     while (start<end) {
+        
         char  temp= s[start];
         s[start]=s[end];
         s[end]=temp;
@@ -15,6 +24,8 @@ public class ReveseString {
         end--;
         
     }
+}
+    return new String(s);
     }
     
 }
